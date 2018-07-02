39f6d2b3f9e0d5195a540e02a6dbbe382533cd9f
khfaith <kh_faith@qq.com>
2018-07-02 19:01:15 +0800

    finished

--- module1/ngx_http_mytest_module.c
+++ module1/ngx_http_mytest_module.c
@@ -6,10 +6,12 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 #include "func.h"
+#include "cJSON.h"
 typedef struct{
 	u_char *session;
 	u_char *data;
 	u_char *requesttime;
+	u_char *ser_data;
 	u_char key[33];
 	struct timeval start;
 	struct timeval end;
@@ -73,7 +75,53 @@ ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 	clcf->handler = ngx_http_mytest_handler;
 
 	return NGX_CONF_OK;
-}		
+}
+
+ngx_int_t cJSON_to_array(char *text,u_char *key,u_char *session,ngx_http_request_t *r)
+{
+	cJSON *json,*arrayItem,*item,*object;
+	ngx_int_t i;
+
+	json=cJSON_Parse(text);
+	if (!json)
+	{
+		ngx_log_stderr(0,"Error before: [%s]\n",cJSON_GetErrorPtr());
+	}
+	else
+	{
+		arrayItem=cJSON_GetObjectItem(json,"token");
+		if(arrayItem!=NULL)
+		{
+			int size=cJSON_GetArraySize(arrayItem);
+			ngx_log_stderr(0,"cJSON_GetArraySize: size=%d\n",size);
+
+			for(i=0;i<size;i++)
+			{
+				object = cJSON_GetArrayItem(arrayItem,i);
+
+				item = cJSON_GetObjectItem(object,"session");
+				if(item != NULL)
+				{
+					ngx_log_stderr(0,"item: %s",item->valuestring);
+					if(0 == ngx_strcmp(item->valuestring,(char *)session))
+					{
+						item = cJSON_GetObjectItem(object,"key");
+						if(item != NULL)
+						{
+							ngx_memzero(key,ngx_strlen(item->valuestring)+1);
+							ngx_memcpy(key,item->valuestring,ngx_strlen(item->valuestring));
+							ngx_log_stderr(0,"key: %s",key);
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	cJSON_Delete(json);
+	return 0;
+}
+
 static ngx_int_t mytest_subrequest_post_handler(ngx_http_request_t *r,void *data, ngx_int_t rc)
 {
 	ngx_http_request_t          *pr = r->parent;
@@ -84,13 +132,35 @@ static ngx_int_t mytest_subrequest_post_handler(ngx_http_request_t *r,void *data
 	{
 		ngx_log_stderr(0,"data: %s",myctx->data);
 	}
+	ngx_buf_t* pRecvBuf = &r->upstream->buffer;
+	
+	ngx_str_t cjson;
+	cjson.len = pRecvBuf->last - pRecvBuf->pos;
+	cjson.data = pRecvBuf->pos;
+
+	cJSON_to_array((char *)cjson.data,myctx->key,myctx->session,r);
+	ngx_log_stderr(0,"myctx->key: %s",myctx->key);
+
+	u_char filename[] = {"/home/faith/ngx_test"};
+	ngx_file_t *file;
+	file = ngx_palloc(r->pool,sizeof(ngx_file_t));
+	file->fd = ngx_open_file(filename,NGX_FILE_WRONLY,NGX_FILE_CREATE_OR_OPEN,0644);
+	if(file->fd < 0){
+		ngx_log_stderr(0,"%s failed",filename);
+		return NGX_ERROR;
+	}
+	ngx_log_stderr(0,"file->fd : %d",file->fd);
+	u_char * buf = ngx_alloc(66,r->pool->log);
+	ngx_snprintf(buf,66,"%s %s\n",myctx->session,myctx->key);
+	ngx_log_stderr(0,"buf : %s",buf);
+	ngx_write_fd(file->fd,buf,66);
+
 	pr->write_event_handler = (void*)mytest_post_handler;
 
 	return NGX_OK;
 }
 
-	static ngx_int_t
-mytest_post_handler(ngx_http_request_t * r)
+static ngx_int_t mytest_post_handler(ngx_http_request_t * r)
 {
 	if (r->headers_out.status != NGX_HTTP_OK)
 	{
@@ -140,7 +210,26 @@ mytest_post_handler(ngx_http_request_t * r)
 		}
 	}
 }
-
+ngx_int_t cJSON_to_str(char *json_string,u_char *serdata,ngx_http_request_t *r)
+{
+	cJSON *root=cJSON_Parse(json_string);
+	if (!root)
+	{
+		ngx_log_stderr(0,"Error before: [%s]\n",cJSON_GetErrorPtr());
+		return -1;
+	}
+	else
+	{
+		cJSON *item=cJSON_GetObjectItem(root,"body");
+		if(item!=NULL)
+		{
+			ngx_memcpy(serdata,item->valuestring,ngx_strlen(item->valuestring));
+			ngx_log_stderr(0,"serdata : %s",serdata);
+		}
+		cJSON_Delete(root);
+	}
+	return 0;
+}
 static ngx_int_t mytest_subrequest_post_key_handler(ngx_http_request_t *r,void *data, ngx_int_t rc)
 {
 	//当前请求r是子请求，它的parent成员就指向父请求
@@ -152,6 +241,16 @@ static ngx_int_t mytest_subrequest_post_key_handler(ngx_http_request_t *r,void *
 	{
 		ngx_log_stderr(0,"key: %s",myctx->key);
 	}
+	ngx_buf_t* pRecvBuf = &r->upstream->buffer;
+
+	ngx_str_t cjson;
+	cjson.len = pRecvBuf->last - pRecvBuf->pos;
+	cjson.data = pRecvBuf->pos;
+
+	myctx->ser_data = ngx_alloc(1024,r->pool->log);
+	ngx_memzero(myctx->ser_data,1024);
+	cJSON_to_str((char *)cjson.data,myctx->ser_data,r);
+	ngx_log_stderr(0,"myctx->ser_data : %s",myctx->ser_data);
 	pr->write_event_handler = mytest_post_key_handler;
 
 	return NGX_OK;
@@ -167,9 +266,10 @@ static void mytest_post_key_handler(ngx_http_request_t * r)
 	}	
 	//当前请求是父请求，直接取其上下文
 	ngx_http_mytest_ctx_t* myctx = ngx_http_get_module_ctx(r, ngx_http_mytest_module);
-	u_char *p = ngx_alloc(4096,r->pool->log);
-	ngx_memzero(p,4096);
-	ngx_memcpy(p,"192.168.178.128 faith 121212",28);
+	u_char *p = ngx_alloc(ngx_strlen(myctx->ser_data)+1,r->pool->log);
+	ngx_memzero(p,ngx_strlen(myctx->ser_data)+1);
+	ngx_memcpy(p,myctx->ser_data,ngx_strlen(myctx->ser_data));
+	ngx_log_stderr(0,"p: %s",p);
 	ngx_int_t plen = ngx_strlen(p);
 	if(plen == 0){
 		ngx_log_stderr(0,"明文字符长度不为空！\n");
@@ -218,10 +318,10 @@ static void mytest_post_key_handler(ngx_http_request_t * r)
 	r->connection->buffered |= NGX_HTTP_WRITE_BUFFERED;
 	ngx_int_t ret=ngx_http_send_header(r);
 	ret=ngx_http_output_filter(r, &out);
-	
+
 	ngx_gettimeofday(&myctx->end);
 	ngx_int_t time = (myctx->end.tv_sec - myctx->start.tv_sec)*1000 
-				   + (myctx->end.tv_usec - myctx->start.tv_usec)/1000;
+		+ (myctx->end.tv_usec - myctx->start.tv_usec)/1000;
 	ngx_log_stderr(0,"time: %d",time);
 	ngx_http_finalize_request(r, ret);
 }
@@ -256,11 +356,10 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 	query_string(r,myctx->requesttime,str3);	 //获取requesttime
 
 	u_char filename[] = {"/home/faith/ngx_test"};
-	read_file(r,filename,myctx->session,myctx->key);  //根据session，在文件中查找key
+	ngx_int_t flag = read_file(r,filename,myctx->session,myctx->key);  //根据session，在文件中查找key
 
-	if(ngx_strlen(myctx->key) == 0) //进入token
+	if(flag == -1) //进入token
 	{
-		// ngx_http_post_subrequest_t结构体会决定子请求的回调方法，参见5.4.1节
 		ngx_http_post_subrequest_t *psr = ngx_palloc(r->pool, sizeof(ngx_http_post_subrequest_t));
 		if (psr == NULL)
 		{
@@ -274,9 +373,9 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 		//时传入的data参数就是myctx
 		psr->data = myctx;
 
-		ngx_str_t sub_prefix = ngx_string("/rest/index");
+		ngx_str_t sub_prefix = ngx_string("/rest/index2");
 		ngx_str_t sub_location;
-		sub_location.len = sub_prefix.len + r->args.len;
+		sub_location.len = sub_prefix.len;
 		sub_location.data = ngx_palloc(r->pool, sub_location.len);
 		ngx_snprintf(sub_location.data, sub_location.len,
 				"%V", &sub_prefix);
@@ -337,11 +436,13 @@ ngx_int_t deaes_data(ngx_http_request_t *r,u_char *data,u_char *requesttime,u_ch
 
 	pencode.len = ngx_strlen(data);
 	pencode.data = ngx_alloc(pencode.len + 1,r->pool->log);
+	ngx_memzero(pencode.data,pencode.len+1);
 	ngx_memcpy(pencode.data,data,pencode.len);
 	ngx_log_stderr(0,"pencode: %s",pencode.data);
 
 	pdecode.len = ngx_base64_decoded_length(pencode.len);
 	pdecode.data = ngx_alloc(pdecode.len + 1,r->pool->log);
+	ngx_memzero(pdecode.data,pdecode.len+1);
 	ngx_decode_base64(&pdecode,&pencode);     //base64解码
 	ngx_log_stderr(0,"pdecode: %s",pdecode.data);
 
@@ -416,14 +517,14 @@ ngx_int_t read_file(ngx_http_request_t *r,u_char *realfilename,u_char *session,u
 			lseek(file->fd,filelen,SEEK_SET);
 			n = ngx_read_fd(file->fd,buf,len-1);
 			ngx_log_stderr(0,"n: %d",n);
-			ngx_log_stderr(0,"key : %s",buf);
 			ngx_memcpy(key,buf,len);
-			break;
+			ngx_log_stderr(0,"key : %s",key);
+			return 0;
 		}
 		filelen += 2 * (off_t)len;
 		lseek(file->fd,filelen,SEEK_SET);
 	}
-	return 0;
+	return -1;
 }
 
 
