8236b2b55eda86b6807f1572a005d106e194369b
khfaith <kh_faith@qq.com>
2018-07-09 09:25:40 +0800

    remove

--- module/key/aes.c
+++
@@ -1,8 +0,0 @@
- ///
- /// @file    aes.c
- /// @author  faith(kh_faith@qq.com)
- /// @date    2018-06-20 17:33:56
- ///
- 
-#include <iostream>
-using namespace std;
--- module/key/daes.c
+++
@@ -1,8 +0,0 @@
- ///
- /// @file    daes.c
- /// @author  faith(kh_faith@qq.com)
- /// @date    2018-06-20 17:34:08
- ///
- 
-#include <iostream>
-using namespace std;
